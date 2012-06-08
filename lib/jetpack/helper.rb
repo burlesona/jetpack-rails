63773132e9110ccfeb1b388267bd0e18f6f9d3d5
Andrew Burleson <andrew@eighty-b.com>
2012-06-08 15:42:36 -0500

    Added flash message helper and stylesheets tweak.

--- lib/jetpack/helper.rb
+++ lib/jetpack/helper.rb
@@ -2,9 +2,17 @@ require 'jetpack/version'
 
 module Jetpack
 	module Helper
-		
 		def view_name
 			[controller.controller_name,controller.action_name,params[:id]].compact.join(' ')
+		end
+
+		def flash_messages
+			str = ""
+			flash.each do |type, msg|
+				type == :alert ? persist = true : persist = false
+				str += content_tag :div, msg, :class => ['alert_message', type], :data => { :persist => persist }
+			end
+			str.html_safe
 		end
 	end
 
