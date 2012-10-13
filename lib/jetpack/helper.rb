e122618e78d2899caff7f6c9893d8113608ea1a5
Andrew Burleson <burlesona@gmail.com>
2012-10-13 14:26:56 -0500

    Added title helper.

--- lib/jetpack/helper.rb
+++ lib/jetpack/helper.rb
@@ -2,10 +2,20 @@ require 'jetpack/version'
 
 module Jetpack
 	module Helper
-		
+		# Creates a view name for CSS namespacing.
 		def view_name
 			[controller.controller_name,controller.action_name,params[:id]].compact.join(' ')
 		end
-	end
 
+		# Creates a smart title helper, appending [DEV] when you're in dev mode.
+		def title(page_title)
+			APP_NAME = @app_name unless defined?(APP_NAME)
+			mode = "[DEV] " unless Rails.env.production?
+			if page_title
+				content_for(:title) { mode.to_s + page_title + " | " + APP_NAME }
+			else
+				content_for(:title) { mode.to_s + APP_NAME }
+			end
+		end
+	end
 end
