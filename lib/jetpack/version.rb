21ee1dd52d8352da8ba51906699e0ba142b52b1e
Andrew Burleson <andrew.burleson@citrusbyte.com>
2013-08-15 13:25:13 -0400

    Fix bug with default js

--- lib/jetpack/version.rb
+++ lib/jetpack/version.rb
@@ -1,3 +1,3 @@
 module Jetpack
-	VERSION = "0.9.1"
+	VERSION = "0.9.2"
 end
--- vendor/assets/javascripts/jetpack/alerts.js
+++ vendor/assets/javascripts/jetpack/alerts.js
@@ -1,7 +1,7 @@
 $(function() {
-	$('div.alert_message[data-persist=false]').delay(2000).slideUp();
-	$('div.alert_message[data-persist=true]').append('<a href="#" class="close">x</a>');
-	$('div.alert_message[data-persist=true] a.close').click( function( event ) {
+	$('.alert-message[data-persist=false]').delay(2000).slideUp();
+	$('.alert-message[data-persist=true]').append('<a href="#" class="close">x</a>');
+	$('.alert-message[data-persist=true] a.close').click( function( event ) {
 		$(this).parent().slideUp();
 		event.preventDefault();
 	});
